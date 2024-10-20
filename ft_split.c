147738a00a0dd2cd56c6630c6bff6663530c9571
Melissa Ferreira <melferre@2D2.42angouleme.fr>
2024-10-20 14:40:48 +0200

    up

--- ft_split.c
+++ ft_split.c
@@ -3,45 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bcarolle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: melferre <melferre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 21:34:51 by melferre          #+#    #+#             */
-/*   Updated: 2024/10/20 00:50:44 by bcarolle         ###   ########.fr       */
+/*   Updated: 2024/10/20 14:28:38 by melferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_countwords(char const *s, char c)
+int	ft_countwords(char const *s, char c) //alfonso
 {
 	size_t	j;
 	size_t	i;
-	char	last;
 
 	j = 0;
 	i = 0;
-	last = c;
+	while (s[i] && s[i] == c)
+		i++;
+	if (s[i])
+	{
+		if (s[i] != c)
+			j++;
+		i++;
+	}
 	while (s[i])
 	{
-		if (last == c && s[i] != c)
+		if (s[i - 1] == c && s[i] != c)
 			j++;
-		last = s[i];
 		i++;
 	}
 	return (j);
 }
-int	ft_length_word(char const *s, char c)
+size_t	ft_length_word(char const *s, char c) //en paix alfonso
 {
-	size_t i;
+	size_t	i;
 	
 	i = 0;
-	while(s[i] && s[i] != c)
-	{
+	while (s[i] && s[i] != c)
 		i++;
-	}
 	return (i);
 }
-char *ft_cpyword(char *dest, const char *src, char c)
+char *ft_cpyword(char *dest, const char *src, char c) //alfonso
 {
 	size_t	i;
 
@@ -53,6 +56,14 @@ char *ft_cpyword(char *dest, const char *src, char c)
 	}
 	dest[i] = '\0';
 	return (dest);
+}
+
+char	**multi_free(char **s, size_t j)
+{
+	while (--j != (size_t)(-1))
+		free(s[j]);
+	free(s);
+	return (NULL);
 }
 
 char **ft_split(char const *s, char c)
@@ -69,30 +80,16 @@ char **ft_split(char const *s, char c)
 	split_str[ft_countwords(s, c)] = NULL;
 	while (s[i])
 	{
+		while (s[i] && s[i] == c)
+			i++;
 		if (ft_length_word(&s[i], c) == 0)
 			break;
 		split_str[j] = malloc ((ft_length_word(&s[i], c) + 1) * sizeof (char)); //CALCULER LONGUEUR MOT DU CDIIII
-		if (!split_str)
-			return (NULL);
+		if (!split_str[j])
+			return (multi_free(split_str, j + 1));
 		split_str[j] = ft_cpyword(split_str[j], &s[i], c);
 		j++;
 		i = i + ft_length_word(&s[i], c);
-		while (s[i] == c)
-			i++;
 	}
 	return (split_str);
 }
-
-// #include <stdio.h>
-// int main()
-// {
-// 	char **benoit;
-// 	size_t i = 0;
-	
-// 	benoit = ft_split("Hello           World !", ' ');
-// 	while (benoit[i])
-// 	{
-// 		printf("%s\n", benoit[i]);
-// 		i++;
-// 	}
-// }
